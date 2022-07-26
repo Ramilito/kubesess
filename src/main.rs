0b91f39148143074d300bfb21bb81c8c5863de4b
Rami Daghlawi <rami.daghlawi@gmail.com>
2022-07-26 21:34:19 +0200

    fix refresh

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,35 @@
-use std::{ process::{Command, Stdio}, io };
+use std::{
+    process::{Command, Stdio}, io,
+};
+
+// use crossterm::{cursor, execute, terminal::EnterAlternateScreen};
+// use tui::{backend::CrosstermBackend, Terminal};
 
 fn main() -> Result<(), io::Error> {
-    let mut kubectx = Command::new("kubectl")
+    let mut contexts = Command::new("kubectl")
         .args(["config", "get-contexts", "-o", "name"])
         .stdin(Stdio::piped())
         .stdout(Stdio::piped())
         .spawn()
         .unwrap();
 
-    let stdout = Command::new("fzf")
+    let choice = Command::new("fzf")
         .args(["--ansi", "--no-preview"])
-        .stdin(kubectx.stdout.take().unwrap())
+        .stdin(contexts.stdout.take().unwrap())
         .stdout(Stdio::piped())
-        .spawn()?;
+        .spawn()?
+        .wait_with_output()?;
 
-    let output = stdout.wait_with_output()?;
-    let result = String::from_utf8(output.stdout).unwrap();
-
-    Command::new("kubectl")
-        .args(["config", "use-context", &result.trim()])
+    let result = Command::new("kubectl")
+        .args([
+            "config",
+            "use-context",
+            String::from_utf8(choice.stdout).unwrap().trim(),
+        ])
         .stdin(Stdio::piped())
         .stdout(Stdio::piped())
-        .spawn()
-        .unwrap();
+        .spawn()?
+        .wait()?;
 
     println!("{}", result);
 
