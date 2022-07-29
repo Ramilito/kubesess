800f9b800b4f453842ac3f8dde031196fb914a65
Rami Daghlawi <rami.daghlawi@gmail.com>
2022-07-30 01:05:09 +0200

    better dir solution

--- run.sh
+++ run.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
-NEW_CTX=$($parent_path/kubesess/kubesess)
+pushd $HOME/kubesess
+
+NEW_CTX=$(./kubesess)
+
+popd
 
 export KUBECONFIG=$NEW_CTX:$KUBECONFIG
