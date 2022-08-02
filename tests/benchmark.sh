5c19232f7decaef2cfecdf141093336ff9ae3ec8
Rami Daghlawi <rami.daghlawi@gmail.com>
2022-08-02 09:45:40 +0200

    fix tests

--- tests/benchmark.sh
+++ tests/benchmark.sh
@@ -1,10 +1,10 @@
 timekubesess(){
   start=`date +%s.%N`
   for i in $(seq 1 20); do
-    export KUBECONFIG=$(~/kubesess/kubesess -- context other-cluster):$HOME/.kube/config;
-    # k get nodes;
-    export KUBECONFIG=$(~/kubesess/kubesess -- context docker-desktop):$HOME/.kube/config;
-    # k get nodes;
+    export KUBECONFIG=$(~/kubesess/kubesess -c arn:aws:eks:eu-north-1:392162365071:cluster/toca-days-staging-eu-north-1 context):$HOME/.kube/config;
+    kubectl get nodes > /dev/null 2>&1;
+    export KUBECONFIG=$(~/kubesess/kubesess -c docker-desktop context):$HOME/.kube/config;
+    kubectl get nodes > /dev/null 2>&1;
   done
   end=`date +%s.%N`
   echo time
@@ -15,13 +15,22 @@ timekubesess(){
 timekubectx(){
   start=`date +%s.%N`
   for i in $(seq 1 20); do 
-    kubectx other-cluster > /dev/null 2>&1;
-    # k get nodes;
+    kubectx arn:aws:eks:eu-north-1:392162365071:cluster/toca-days-staging-eu-north-1 > /dev/null 2>&1;
+    kubectl get nodes > /dev/null 2>&1;
     kubectx docker-desktop > /dev/null 2>&1; 
-    # k get nodes;
+    kubectl get nodes > /dev/null 2>&1;
   done
   end=`date +%s.%N`
   echo time
   runtime=$( echo "$end - $start" | bc -l )
   echo $runtime
 }
+
+
+echo "Starting kubesess test:"
+timekubesess
+echo "Ending kubesess test:"
+
+echo "Starting kubectx test:"
+timekubectx
+echo "Ending kubectx test:"
