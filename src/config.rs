3d664ab2e989e5c16fc4c2e63886bb22d0d3419f
Rami Daghlawi <rami.daghlawi@gmail.com>
2022-08-04 07:50:21 +0200

    refactored config.rs code

--- src/config.rs
+++ src/config.rs
@@ -1,6 +1,6 @@
 use serde::{Deserialize, Serialize};
 use std::{
-    fs,
+    fs::{self, File},
     io::{BufRead, BufReader, BufWriter},
     path::Path,
 };
@@ -32,25 +32,7 @@ struct Config {
     contexts: Vec<Contexts>,
 }
 
-pub fn create_file(ctx: &String, namespace: Option<&String>, temp_dir: &str) {
-    let path = Path::new(ctx);
-    let parent = path.parent().unwrap();
-    let dirname = str::replace(&parent.display().to_string(), ":", "_");
-    let filename = path.file_name().unwrap().to_str().unwrap();
-    let _ = fs::create_dir_all(format!("{}/{}", temp_dir, dirname));
-
-    let f = std::fs::OpenOptions::new()
-        .read(true)
-        .write(true)
-        .create(true)
-        .open(format!("{}/{}/{}", temp_dir, dirname, filename))
-        .expect("Unable to open file");
-
-    let mut reader = BufReader::new(&f);
-    let mut strbuf = String::new();
-
-    reader.read_line(&mut strbuf).expect("Unable to read file");
-
+fn build_config(ctx: &String, namespace: Option<&String>, strbuf: String) -> Config {
     let mut config: Config = serde_yaml::from_str(&strbuf).unwrap();
     config.api_version = "v1".to_string();
     config.kind = "Config".to_string();
@@ -67,13 +49,40 @@ pub fn create_file(ctx: &String, namespace: Option<&String>, temp_dir: &str) {
         }];
     }
 
+    config
+}
+
+fn get_config(ctx: &String, temp_dir: &str, mut strbuf: String) {
+    let f = get_config_file(ctx, temp_dir);
+    let mut reader = BufReader::new(&f);
+
+    reader.read_line(&mut strbuf).expect("Unable to read file");
+}
+
+fn get_config_file(ctx: &String, temp_dir: &str) -> File {
+    let path = Path::new(ctx);
+    let parent = path.parent().unwrap();
+    let dirname = str::replace(&parent.display().to_string(), ":", "_");
+    let filename = path.file_name().unwrap().to_str().unwrap();
+    let _ = fs::create_dir_all(format!("{}/{}", temp_dir, dirname));
+
     let f = std::fs::OpenOptions::new()
         .read(true)
         .write(true)
         .create(true)
         .open(format!("{}/{}/{}", temp_dir, dirname, filename))
         .expect("Unable to open file");
+    f
+}
 
+pub fn set(ctx: &String, namespace: Option<&String>, temp_dir: &str) {
+    let strbuf = String::new();
+
+    get_config(ctx, temp_dir, strbuf.to_owned());
+
+    let f = get_config_file(ctx, temp_dir);
     let writer = BufWriter::new(&f);
+    let config = build_config(ctx, namespace, strbuf);
+    println!("{:?}", config);
     serde_yaml::to_writer(writer, &config).unwrap();
 }
