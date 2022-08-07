c739c2fb68414887108c4ad29bba20fb133d3261
Rami Daghlawi <rami.daghlawi@gmail.com>
2022-08-07 10:33:16 +0200

    added comments to script

--- kubesess.sh
+++ kubesess.sh
@@ -1,15 +1,17 @@
 kc() {
   config=$(kubesess ${1:+"-v "$1} context);
+  # Prepend the $KUBECONFIG with new config
   export KUBECONFIG=$config:$HOME/.kube/config;
 }
 
 kcd() {
   kubesess ${1:+"-v "$1} default-context;
+  # Reset the KUBECONFIG variable
   export KUBECONFIG=$HOME/.kube/config;
 }
 
 kn() {
   config=$(kubesess ${1:+"-v "$1} namespace);
+  # Prepend the $KUBECONFIG with new config
   export KUBECONFIG=$config:$HOME/.kube/config;
-
 }
