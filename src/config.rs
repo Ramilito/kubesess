cfa5ef46f4e7bc84af94cfe5b0167543bb2ae4bc
Rami Daghlawi <rami.daghlawi@gmail.com>
2022-08-23 23:31:26 +0200

    removed shared model, using new config struct

--- src/config.rs
+++ src/config.rs
@@ -1,59 +1,31 @@
-use serde::{Deserialize, Serialize};
+use crate::model::{Config, Context, Contexts};
+
 use std::{
     fs::{self, File},
     io::{BufRead, BufReader, BufWriter},
     path::Path,
 };
 
-#[derive(Default, Debug, Serialize, Deserialize)]
-struct Contexts {
-    context: Context,
-    name: String,
-}
-
-#[derive(Default, Debug, Serialize, Deserialize)]
-struct Context {
-    namespace: String,
-    cluster: String,
-    user: String,
-}
-
-#[derive(Debug, Serialize, Deserialize)]
-struct Config {
-    #[serde(skip_serializing_if = "String::is_empty", default)]
-    kind: String,
-    #[serde(rename = "apiVersion")]
-    #[serde(skip_serializing_if = "String::is_empty", default)]
-    api_version: String,
-    #[serde(skip_serializing_if = "String::is_empty", default)]
-    #[serde(rename = "current-context")]
-    current_context: String,
-    #[serde(skip_serializing_if = "Vec::is_empty", default)]
-    contexts: Vec<Contexts>,
-}
-
-fn build_config(ctx: &str, namespace: Option<&str>, strbuf: &str) -> Config {
+fn build_config(ctx: &Contexts, ns: Option<&str>, strbuf: &str) -> Config {
     let mut config: Config = serde_yaml::from_str(&strbuf).unwrap();
     config.api_version = "v1".to_string();
     config.kind = "Config".to_string();
-    config.current_context = format!("{}", ctx);
+    config.current_context = format!("{}", ctx.name);
 
-    if let Some(ns) = namespace {
-        config.contexts = vec![Contexts {
-            context: Context {
-                namespace: ns.to_string(),
-                cluster: ctx.to_string(),
-                user: ctx.to_string(),
-            },
-            name: ctx.to_string(),
-        }];
-    }
+    config.contexts = vec![Contexts {
+        context: Context {
+            namespace: ns.unwrap_or("default").to_string(),
+            cluster: ctx.context.cluster.to_string(),
+            user: ctx.context.user.to_string(),
+        },
+        name: ctx.name.to_string(),
+    }];
 
     config
 }
 
-fn get_config_file(ctx: &str, dest: &str) -> File {
-    let path = Path::new(ctx);
+fn get_config_file(ctx: &Contexts, dest: &str) -> File {
+    let path = Path::new(ctx.name.as_str());
     let parent = path.parent().unwrap();
     let dirname = str::replace(&parent.display().to_string(), ":", "_");
     let _ = fs::create_dir_all(format!("{}/{}", dest, dirname));
@@ -62,6 +34,7 @@ fn get_config_file(ctx: &str, dest: &str) -> File {
         .read(true)
         .write(true)
         .create(true)
+        .truncate(true)
         .open(format!(
             "{}/{}/{}",
             dest,
@@ -72,9 +45,9 @@ fn get_config_file(ctx: &str, dest: &str) -> File {
     f
 }
 
-pub fn set(ctx: &str, namespace: Option<&str>, dest: &str) {
+pub fn set(ctx: &Contexts, namespace: Option<&str>, dest: &str) {
     let strbuf = String::new();
-    let options = get_config_file(ctx, dest);
+    let options = get_config_file(&ctx, dest);
     let mut reader = BufReader::new(&options);
 
     reader
@@ -82,7 +55,7 @@ pub fn set(ctx: &str, namespace: Option<&str>, dest: &str) {
         .expect("Unable to read file");
 
     let writer = BufWriter::new(&options);
-    let config = build_config(ctx, namespace, strbuf.as_str());
+    let config = build_config(&ctx, namespace, strbuf.as_str());
 
     serde_yaml::to_writer(writer, &config).unwrap();
 }
