e50769019155d98dbdf3a7dd39aad7583aa70b0d
Rami Daghlawi <rami.daghlawi@gmail.com>
2022-08-23 23:31:06 +0200

    modes uses new config

--- src/modes.rs
+++ src/modes.rs
@@ -8,39 +8,59 @@ fn selection(value: Option<String>, callback: fn() -> String) -> String {
 }
 
 pub fn default_context(args: Cli, dest: &str) {
-    let ctx = selection(args.value, || -> String {
-        let contexts = commands::get_context();
-        commands::selectable_list(contexts)
-    });
+    let config = commands::get_config();
+
+    let ctx = match args.value {
+        None => {
+            let mut options = Vec::new();
+            for context in &config.contexts {
+                options.push(context.name.to_string());
+            }
+
+            commands::selectable_list(options)
+        }
+        Some(x) => x.trim().to_string(),
+    };
 
     commands::set_default_context(&ctx);
-    commands::set_context(&ctx, &dest);
+    commands::set_context(&ctx, &dest, &config);
 }
 
 pub fn context(args: Cli, dest: &str) {
-    let ctx = selection(args.value, || -> String {
-        let contexts = commands::get_context();
-        commands::selectable_list(contexts)
-    });
+    let config = commands::get_config();
 
-    commands::set_context(&ctx, &dest);
+    let ctx = match args.value {
+        None => {
+            let mut options = Vec::new();
+            for context in &config.contexts {
+                options.push(context.name.to_string());
+            }
+
+            commands::selectable_list(options)
+        }
+        Some(x) => x.trim().to_string(),
+    };
+
+    commands::set_context(&ctx, &dest, &config);
 
     println!("{}/{}", &dest, str::replace(&ctx, ":", "_"));
 }
 
 pub fn namespace(args: Cli, dest: &str) {
+    let config = commands::get_config();
     let ctx = commands::get_current_context();
     let ns = selection(args.value, || -> String {
         let namespaces = commands::get_namespaces();
         commands::selectable_list(namespaces)
     });
 
-    commands::set_namespace(&ctx, &ns, &dest);
+    commands::set_namespace(&ctx, &ns, &dest, &config);
 
     println!("{}/{}", &dest, str::replace(&ctx, ":", "_"));
 }
 
 pub fn default_namespace(args: Cli, dest: &str) {
+    let config = commands::get_config();
     let ctx = commands::get_current_context();
     let ns = selection(args.value, || -> String {
         let namespaces = commands::get_namespaces();
@@ -48,5 +68,5 @@ pub fn default_namespace(args: Cli, dest: &str) {
     });
 
     commands::set_default_namespace(&ns);
-    commands::set_namespace(&ctx, &ns, &dest);
+    commands::set_namespace(&ctx, &ns, &dest, &config);
 }
