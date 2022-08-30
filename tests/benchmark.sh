76992858d3ccffa5a99faf6ec851c8f267d6e6f6
Ramilito <rami.daghlawi@gmail.com>
2022-08-30 04:23:28 +0200

    Update benchmark.sh

--- tests/benchmark.sh
+++ tests/benchmark.sh
@@ -1,9 +1,9 @@
 timekubesess(){
   start=`date +%s.%N`
   for i in $(seq 1 20); do
-    export KUBECONFIG=$(kubesess -v arn:aws:eks:eu-north-1:392162365071:cluster/toca-days-staging-eu-north-1 context):$HOME/.kube/config;
-    kubectl get nodes;
     export KUBECONFIG=$(kubesess -v docker-desktop context):$HOME/.kube/config;
+    kubectl get nodes;
+    export KUBECONFIG=$(kubesess -v docker-desktop-2 context):$HOME/.kube/config;
     kubectl get nodes;
   done
   end=`date +%s.%N`
@@ -15,9 +15,9 @@ timekubesess(){
 timekubectx(){
   start=`date +%s.%N`
   for i in $(seq 1 20); do 
-    kubectx arn:aws:eks:eu-north-1:392162365071:cluster/toca-days-staging-eu-north-1 > /dev/null 2>&1;
+    kubectx docker-desktop > /dev/null 2>&1;
     kubectl get nodes;
-    kubectx docker-desktop > /dev/null 2>&1; 
+    kubectx docker-desktop-2 > /dev/null 2>&1; 
     kubectl get nodes;
   done
   end=`date +%s.%N`
