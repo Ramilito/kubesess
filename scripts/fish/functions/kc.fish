bf85211e6fdb9c32b0b12b9a2fc40ad2d4013e85
Sergei Morozov <morozov@tut.by>
2022-10-29 17:55:50 -0700

    Fix Fish functions

--- scripts/fish/functions/kc.fish
+++ scripts/fish/functions/kc.fish
@@ -1,4 +1,8 @@
 function kc --argument-names context --description "Switch current kubernetes context"
-    set -l config (kubesess -v $context context) 
+    set -l cmd kubesess context
+    if test -n "$argv"
+        set -a cmd -v $context
+    end
+    set -l config (command $cmd) || return $status
     set -gx KUBECONFIG $config
 end
