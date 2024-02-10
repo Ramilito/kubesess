522af07674368e42bb9f8f983009c3e674a963a8
Rami Daghlawi <rami.daghlawi@gmail.com>
2024-02-10 20:56:45 +0100

    propagate original config

--- src/commands.rs
+++ src/commands.rs
@@ -89,12 +89,12 @@ pub fn selectable_list(input: Vec<String>) -> String {
 
 pub fn set_namespace(ctx: &str, selection: &str, temp_dir: &str, config: &KubeConfig) {
     let choice = config.contexts.iter().find(|x| x.name == ctx);
-    config::write(choice.unwrap(), Some(selection), temp_dir)
+    config::write(choice.unwrap(), config, Some(selection), temp_dir)
 }
 
 pub fn set_context(ctx: &str, temp_dir: &str, config: &KubeConfig) -> Result<(), SetContextError> {
     if let Some(choice) = config.contexts.iter().find(|x| x.name == ctx) {
-        config::write(choice, None, temp_dir);
+        config::write(choice, config, None, temp_dir);
         Ok(())
     } else {
         Err(SetContextError::ContextNotFound{ctx: ctx.to_owned()})
