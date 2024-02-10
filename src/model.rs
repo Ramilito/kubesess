ddd8ca5f6203f8e80bf16e8b10af5f9e5692ebc3
Rami Daghlawi <rami.daghlawi@gmail.com>
2024-02-10 20:56:33 +0100

    add new models

--- src/model.rs
+++ src/model.rs
@@ -1,4 +1,7 @@
+use std::collections::HashMap;
+
 use serde::{Deserialize, Serialize};
+use serde_yaml::Value;
 
 #[derive(Default, PartialEq, Debug, Serialize, Deserialize)]
 pub struct Contexts {
@@ -6,6 +9,38 @@ pub struct Contexts {
     pub context: Context,
     #[serde(default)]
     pub name: String,
+}
+
+#[derive(Default, Clone, PartialEq, Debug, Serialize, Deserialize)]
+pub struct Clusters {
+    #[serde(default)]
+    pub cluster: Cluster,
+    #[serde(default)]
+    pub name: String,
+}
+#[derive(Default, Clone, PartialEq, Debug, Serialize, Deserialize)]
+pub struct Cluster {
+    #[serde(rename = "certificate-authority-data")]
+    pub certificate_authority_data: String,
+    pub server: String,
+}
+
+#[derive(Default, Clone, PartialEq, Debug, Serialize, Deserialize)]
+pub struct Users {
+    #[serde(default)]
+    pub user: HashMap<String, Value>,
+    #[serde(default)]
+    pub name: String,
+}
+
+#[derive(Default, PartialEq, Debug, Serialize, Deserialize)]
+pub struct User {
+    #[serde(skip_serializing_if = "String::is_empty", default)]
+    #[serde(rename = "client-certificate-data")]
+    pub client_certificate_data: String,
+    #[serde(skip_serializing_if = "String::is_empty", default)]
+    #[serde(rename = "client-key-data")]
+    pub client_key_data: String,
 }
 
 #[derive(Default, PartialEq, Debug, Serialize, Deserialize)]
@@ -28,4 +63,8 @@ pub struct KubeConfig {
     pub current_context: String,
     #[serde(default)]
     pub contexts: Vec<Contexts>,
+    #[serde(default)]
+    pub users: Vec<Users>,
+    #[serde(default)]
+    pub clusters: Vec<Clusters>,
 }
