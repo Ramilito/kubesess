7ad23f6722d89b9d2877abb7a226432410422649
Rami Daghlawi <rami.daghlawi@gmail.com>
2024-10-19 07:56:18 +0200

    removed error

--- src/error.rs
+++ src/error.rs
@@ -5,15 +5,17 @@ pub enum Error {
     #[error("failed to set context: {0}")]
     SetContext(#[source] SetContextError),
     #[error("no item selected when prompted to select {prompt}")]
-    NoItemSelected {
-        prompt: &'static str 
-    },
+    NoItemSelected { prompt: &'static str },
 }
+
+// #[derive(Error, Debug)]
+// pub enum SetNamespaceError {
+//     #[error("no namespace exists with the name {ns}")]
+//     KubeNamespaceNotFound { ns: String },
+// }
 
 #[derive(Error, Debug)]
 pub enum SetContextError {
     #[error("no context exists with the name {ctx}")]
-    KubeContextNotFound {
-        ctx: String
-    },
+    KubeContextNotFound { ctx: String },
 }
