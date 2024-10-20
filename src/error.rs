c19873c494a04140cc3e9ecf6e52166a96e0b3be
Rami Daghlawi <rami.daghlawi@gmail.com>
2024-10-20 23:29:27 +0200

    cleanup code

--- src/error.rs
+++ src/error.rs
@@ -8,12 +8,6 @@ pub enum Error {
     NoItemSelected { prompt: &'static str },
 }
 
-// #[derive(Error, Debug)]
-// pub enum SetNamespaceError {
-//     #[error("no namespace exists with the name {ns}")]
-//     KubeNamespaceNotFound { ns: String },
-// }
-
 #[derive(Error, Debug)]
 pub enum SetContextError {
     #[error("no context exists with the name {ctx}")]
